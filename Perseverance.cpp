b3e21002fd14a3aa2041e6fae8937fc51c82771f
sellendk <dominik.sellenthin@web.de>
2025-06-24 14:53:54 +0200

    initial commit

---
+++ Perseverance.cpp
@@ -0,0 +1,12 @@
+﻿// Perseverance.cpp : Defines the entry point for the application.
+//
+
+#include "Perseverance.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
